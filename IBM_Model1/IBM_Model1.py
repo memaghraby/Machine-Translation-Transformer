ab8c4e58dd997e3043640a0e312c5935adc0bb2b
AhmedIbrahimAbdellatif <Ahmed.Ahmed99@eng-st.cu.edu.eg>
2022-05-21 13:20:34 +0200

    SMT - Fix and integrate translation table creation

--- IBM_Model1/IBM_Model1.py
+++ IBM_Model1/IBM_Model1.py
@@ -3,6 +3,7 @@ import string
 import pathlib
 import json
 from tensorflow import keras
+from collections import defaultdict
 import nltk
 from nltk.tokenize import word_tokenize
 import numpy as np
@@ -10,8 +11,13 @@ import math
 # nltk.download('punkt')
 
 class IBM:
-  MAX_NUM_OF_ITERATIONS = 2
+  MAX_NUM_OF_ITERATIONS = 10
+  MIN_PROB = 1.0e-12
   EPSILON = 1e-20
+  ENGLISH_SENTENCE_INDEX = 0
+  SPANISH_SENTENCE_INDEX = 1
+  TRAINIG_MODE = True
+  EXECUTE_MODE = False
   def __init__(self, isTrain = False) -> None:
     if isTrain:
       text_file = keras.utils.get_file(
@@ -22,41 +28,53 @@ class IBM:
       self.text_file = pathlib.Path(text_file).parent / "spa-eng" / "spa.txt"
     self.t_spa_en_mat = None
 
+  def build_languages_vocabulary(self, sentences_dataset):
+        spanish_vocab = set()
+        english_vocab = set()
+        for aligned_sentence in sentences_dataset:
+            english_vocab.update(aligned_sentence[IBM.ENGLISH_SENTENCE_INDEX])
+            spanish_vocab.update(aligned_sentence[IBM.SPANISH_SENTENCE_INDEX])
+        # Add the NULL token
+        spanish_vocab.add(None)
+
+        self.spanish_vocab = spanish_vocab
+
+        self.english_vocab = english_vocab
+  def init_probabilities(self):
+        self.translation_table = defaultdict(
+            lambda: defaultdict(lambda: IBM.MIN_PROB)
+        )
+        self.alignment_table = defaultdict(
+            lambda: defaultdict(
+                lambda: defaultdict(lambda: defaultdict(lambda: IBM.MIN_PROB))
+            )
+        )
   
   def train(self):
     print('Henaaa')
     en_sentences, spa_sentences  = self.preprocess()
-    en_tokens, en_dictionary = self.tokenize_sentences(en_sentences)
-    spa_tokens, spa_dictionary = self.tokenize_sentences(spa_sentences)
-    t_en_spa_mat = self.expectation_maximization(spa_dictionary,en_dictionary,spa_tokens,en_tokens)
+    en_tokenized_sentences = self.tokenize_sentences(en_sentences)
+    spa_tokenized_sentences = self.tokenize_sentences(spa_sentences)
+    bilingual_tokenized_text = []
+    for iter in zip(en_tokenized_sentences, spa_tokenized_sentences):
+        bilingual_tokenized_text.append([iter[IBM.ENGLISH_SENTENCE_INDEX],iter[IBM.SPANISH_SENTENCE_INDEX]])
     
-    # Saving the output
-    np.save('t_en_spa_mat', t_en_spa_mat)
+    self.build_languages_vocabulary(bilingual_tokenized_text)
+    self.init_probabilities()
     
-    #Writng language map
-    out_file = open("spanish_index_map.json", "w")
-    json.dump(spa_dictionary, out_file, indent = 6)
+    self.expectation_maximization(bilingual_tokenized_text)
+
+    out_file = open("translation_table.json", "w")
+    json.dump(self.translation_table, out_file, indent = 6)
     out_file.close()
     
-    out_file = open("english_index_map.json", "w")
-    json.dump(en_dictionary, out_file, indent = 6)
-    out_file.close()
 
-  
-    
-
-  def is_converged(self, new,old,num_of_iterations):
+  def is_converged(self, num_of_iterations):
     # print('Checking convergence')
     if num_of_iterations > self.MAX_NUM_OF_ITERATIONS :
+        print("Converged")
         return True
-
-    for i in range(len(new)):
-        for j in range(len(new[0])):
-            if math.fabs(new[i][j]- old[i][j]) > self.EPSILON:
-                return False
-    
-    print('Coverged')
-    return True
+    return False
 
   def preprocess(self):
     with open(self.text_file, encoding="utf8") as f:
@@ -67,116 +85,70 @@ class IBM:
     for line in lines:
         eng, spa = line.split("\t")
         text_pairs.append((spa, eng))
-        if len(eng.split()) < 11 and len(spa.split()) < 11:
-            en_sentences.append(eng)
-            spa_sentences.append(spa)
-    
+        en_sentences.append(eng)
+        spa_sentences.append(spa)
     return en_sentences, spa_sentences
 
   def tokenize_sentences(self, sentence_list):
     tokenized_sentences = []
     translate_table = dict((ord(char), None) for char in string.punctuation)
-    word_dictionary = {}
-    lang_order = 0
     for sentence in sentence_list:
         sentence = sentence.translate(translate_table)
         tokens = word_tokenize(sentence.lower())
-        produced_sentence = ""
-        for token in tokens:
-            if token not in word_dictionary:
-                word_dictionary[token] = lang_order
-                lang_order += 1
-            produced_sentence = produced_sentence + str(token) + " "
-        produced_sentence = produced_sentence[:(
-            len(produced_sentence) - 1)]  # remove last empty
-        tokenized_sentences.append(produced_sentence)
-    return tokenized_sentences, word_dictionary
+        tokenized_sentences.append(tokens)
+    return tokenized_sentences
 
 
-  def expectation_maximization(self, spa_word_dict, en_word_dict, spanish_sentences, english_sentences):
-
-    # Initialize the translation probability matrix
-    t_spa_en_mat = np.full((len(en_word_dict), len(
-        spa_word_dict)), 1/len(spa_word_dict), dtype=float)
-    # print(np.where(t_spa_en_mat == 0))
-    t_spa_en_mat_prev = np.full(
-        (len(en_word_dict), len(spa_word_dict)), 1, dtype=float)
+  def expectation_maximization(self, sentences_dataset):
+    count_en_spa = defaultdict(lambda: defaultdict(lambda: 0.0))
+    total_spa = defaultdict(lambda: 0.0)
+    initial_prob = 1 / len(self.english_vocab)
+    for t in self.english_vocab:
+        self.translation_table[t] = defaultdict(lambda: initial_prob)
 
     num_of_iter = 0
-    while not self.is_converged(t_spa_en_mat, t_spa_en_mat_prev, num_of_iter):
-        print('Iteration : ', num_of_iter)
-        num_of_iter += 1
+    while not self.is_converged(num_of_iter):
+      print('Iteration : ', num_of_iter)
+      num_of_iter += 1
 
-        # Initialization of variables
-        t_spa_en_mat_prev = t_spa_en_mat.copy()
-        count_en_spa = np.full(
-            (len(en_word_dict), len(spa_word_dict)), 0, dtype=float)
-        total_spa = np.full(len(spa_word_dict), 0, dtype=float)
-        e_total = np.full((len(en_word_dict)), 0, dtype=float)
-        for idx_en, en_sent in enumerate(english_sentences):
-            for en_word in en_sent.split():
-                en_index = en_word_dict[en_word]
-                spa_sen_words = spanish_sentences[idx_en].split(" ")
-                for spa_word in spa_sen_words:
-                    spa_index = spa_word_dict[spa_word]
-                    e_total[en_index] += t_spa_en_mat[en_index][spa_index]
-            
-            
-            # collect counts
-            for en_word in en_sent.split():
-                en_index = en_word_dict[en_word]
-                spa_sen_words = spanish_sentences[idx_en].split(" ")
-                for spa_word in spa_sen_words:
-                    count_en_spa[en_index][spa_index] += t_spa_en_mat[en_index][spa_index] / e_total[en_index]
-                    
-                    total_spa[spa_index] += t_spa_en_mat[en_index][spa_index] / e_total[en_index]
+      # Initialization of variables
+      for aligned_sentences in sentences_dataset:
+        english_sentence = aligned_sentences[IBM.ENGLISH_SENTENCE_INDEX]
+        spanish_sentence = aligned_sentences[IBM.SPANISH_SENTENCE_INDEX]
+        e_total = defaultdict(lambda: 0.0)
+        for en_word in english_sentence:
+            for spa_word in spanish_sentence:
+                e_total[en_word] += self.translation_table[en_word][spa_word] 
+        
+        # collect counts
+        for en_word in english_sentence:
+          for spa_word in spanish_sentence:
+              count_en_spa[en_word][spa_word] += self.translation_table[en_word][spa_word] / e_total[en_word]
+              total_spa[spa_word] += self.translation_table[en_word][spa_word] / e_total[en_word]
 
-        print('Finished first loop')
+      print('Finished first loop')
 
-        # estimate probabilities
-        for spa_word in spa_word_dict.keys():
-            for eng_word in en_word_dict.keys():
-                spa_index = spa_word_dict[spa_word]
-                eng_index = en_word_dict[eng_word]
-                if count_en_spa[eng_index][spa_index] != 0:
-                    t_spa_en_mat[eng_index][spa_index] = count_en_spa[en_index][spa_index] / total_spa[spa_index]
+      # estimate probabilities
+      for en_word, spa_words in count_en_spa.items():
+          for spa_word in spa_words:
+            if count_en_spa[en_word][spa_word] != 0:
+                self.translation_table[en_word][spa_word] = count_en_spa[en_word][spa_word] / total_spa[spa_word]
 
-        print("finish ")
+      print("finish ")
     # end while
 
-    # print(t_spa_en_mat)
-
-    return t_spa_en_mat
-
   def load(self):
-
     # Load the language maps
-    with open("spanish_index_map.json", "r") as f:
-      self.spa_dictionary = json.load(f)
-    with open("english_index_map.json", "r") as f:
-      self.en_dictionary = json.load(f)
-
-    print('Loaded Language Maps')
-
-    # loading npy array
-    self.t_spa_en_mat = np.load("t_en_spa_mat.npy")
-    print('Loaded Spanish to English Matrix')
-  
-  def getEnglishDict(self):
-    return self.en_dictionary
-
-  def getSpanishDict(self):
-    return self.spa_dictionary
+    with open("translation_table.json", "r") as f:
+      self.translation_table = json.load(f)
 
   def getTranslationTable(self):
-    return self.t_spa_en_mat
+    return self.translation_table
 
   def translate(self, en_word, spa_word):
-    idx_spa = self.spa_dictionary[spa_word]
-    idx_en = self.en_dictionary[en_word]
-    return self.t_spa_en_mat[idx_en][idx_spa]
+    return self.translation_table[en_word][spa_word]
 
   def test(self):
     print((self.t_spa_en_mat[self.t_spa_en_mat == np.min(self.t_spa_en_mat)]).shape, self.t_spa_en_mat.shape)
 
-IBM().getTranslationTable()
+# IBM(IBM.TRAINIG_MODE).train()
